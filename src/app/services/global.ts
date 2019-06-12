61dfbda11f2a19476c9a69096b0c48d42a35f6b6
jolrobles <jluis4013@gmail.com>
2019-06-12 22:09:45 +0200

    cliente angular validar login

---
+++ src/app/services/global.ts
@@ -0,0 +1,13 @@
+export const Global = {
+    url: 'https://damp-hollows-96576.herokuapp.com/'
+};
+
+// global.spec.ts
+// import { Global } from './global';
+
+// describe('Global', () => {
+//   it('should create an instance', () => {
+//     expect(new Global()).toBeTruthy();
+//   });
+// });
+
